c5b83afb41bbdf214cdfe2238380ffc1448e4233
Andrew <andrew.william03@gmail.com>
2021-05-25 08:18:50 -0300

    faz soma e subtração

--- calculadora/calculadora.py
+++ calculadora/calculadora.py
@@ -1,4 +1,9 @@
 a = int(input("Digite o primeiro valor: "))
 b = int(input("Digite o segundo valor: "))
+operacao = input("+: Soma\n-: Subtração ")
+if operacao == '+':
+	resultado = a + b
+else:
+	resultado = a - b
 resultado = a + b
 print(resultado)
